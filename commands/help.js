85757dcc3712d1f0b8f143c5d4c10eecb95f85ce
JS <astrojord@gmail.com>
2020-08-20 02:57:35 -0400

    added overwatch and expanded help

--- commands/help.js
+++ commands/help.js
@@ -6,19 +6,19 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 		const Discord = require("discord.js");
-        const Math = require("mathjs");
-        const Enmap = require("enmap");
-        const fetch = require('node-fetch');
+    const Math = require("mathjs");
+    const Enmap = require("enmap");
+    const fetch = require('node-fetch');
 		//message.channel.send("get fucked");
 		if (args.length === 0){
 			const e = new Discord.MessageEmbed()
 				.setColor(0xDC143C)
 				.setTimestamp()
-				.setFooter(`⚠ Some commands may require extra arguments. Use >help [name] to show full information.`) // do +command name to show extra info
+				.addField("Available commands:",`\`\`\`f1, ow, yt, remind, ping \`\`\``)
+				.setFooter(`⚠ Some commands may require extra arguments. Use >help [name] to show full information.`); // do +command name to show extra info
 			message.channel.send(e);
 			return;
 		}
-
 		if (args[0].toLowerCase() == `youtube` || args[0].toLowerCase() == `yt`){
 			const e = new Discord.MessageEmbed()
 				.setColor(0xDC143C)
@@ -26,7 +26,31 @@ module.exports = {
 				.addField(`**Information for \`youtube\`:**`, `Returns the top YouTube search result for a specified term/phrase.`)
 				.addField(`Aliases:`,`\`\`\`yt\`\`\``)
 				.addField(`Usage:`,`\`\`\`>youtube [search term]\`\`\``)
-				.addField(`Example:`,`\`\`\`>youtube test video please do not watch\`\`\``)
+				.addField(`Example:`,`\`\`\`>youtube test video please do not watch\`\`\``);
+			message.channel.send(e);
+			return;
+		}
+
+		if (args[0].toLowerCase() == `ow` || args[0].toLowerCase() == `overwatch`){
+			const e = new Discord.MessageEmbed()
+				.setColor(0xDC143C)
+				.setTimestamp()
+				.addField(`**Information for \`ow\`:**`, `Shows Overwatch profile information for a specific Battle.net tag.`)
+				.addField(`Aliases:`,`\`\`\`overwatch\`\`\``)
+				.addField(`Usage:`,`\`\`\`>ow [Battle.net tag] [region] [platform]\`\`\``)
+				.addField(`Defaults:`, `The default region is \`ow\`. The default platform is \`pc\`. Both are optional arguments.`)
+				.addField(`Example:`,`\`\`\`>ow Yakamaru#11162\`\`\``);
+			message.channel.send(e);
+			return;
+		}
+
+		if (args[0].toLowerCase() == `f1` || args[0].toLowerCase() == `formula1`){
+			const e = new Discord.MessageEmbed()
+				.setColor(0xDC143C)
+				.setTimestamp()
+				.addField(`**Information for \`f1\`:**`, `Shows information on the Formula One series for a specific year or race.`)
+				.addField(`Aliases:`,`\`\`\`formula1, formulaone\`\`\``)
+				.addField(`Usage:`,`\`\`\`>f1\n>f1 schedule [year]\n>f1 results [year] [round]\`\`\``);
 			message.channel.send(e);
 			return;
 		}
@@ -37,7 +61,7 @@ module.exports = {
 				.setTimestamp()
 				.addField(`**Information for \`remind\`:**`, `Sets a reminder for yourself -- the bot will ping you with your reminder after the specified time.`)
 				.addField(`Usage:`,`\`\`\`>remind [time] [unit s/m/h/d] [reminder message]\`\`\``)
-				.addField(`Example:`,`\`\`\`>remind 1 h text mom back\`\`\``)
+				.addField(`Example:`,`\`\`\`>remind 1 h text mom back\`\`\``);
 			message.channel.send(e);
 			return;
 		}
--- commands/ow.js
+++ commands/ow.js
@@ -12,27 +12,59 @@ module.exports = {
     const region = (!args[1]) ? "us" : args[1];
     const platform = (!args[2]) ? "pc" : args[2];
 
-    // fix tag to be in correct format for API
-    n = tag.search(/\d+/);
-    tag = (tag[n-1] == '#') ? tag : (tag.slice(0,n) + '-' + tag.slice(n));
-
+    // username no number
+    // number, no username
+    // correct: username#number
+    // correct: username-number
+    // incorrect: username number
+    // incorrect number (an)
 
 		if(!args[0]){
-			message.channel.send("please provide a Battle.net tag to look up!");
+      message.channel.send("please provide a Battle.net tag to look up!");
+      return;
     } else {
-      fetch(`https://ow-api.com/v1/stats/:${platform}/:${region}/:${tag}/profile`)
+      // fix tag to be in correct format for API
+      // get index of first result of #1234 or -1234 (i.e. # or - then 4 digits)
+      const n = tag.search(/(#|-)(\d{4,})/); 
+      if (n === -1) {
+        message.channel.send("the Battle.net tag is incorrectly formatted -- please give in the form of [username]#[numbers]");
+        return;
+      } else {
+        if (tag[n] == '#' || tag[n] == '-'){
+          tag = tag.slice(0, n) + '-' + tag.slice(n+1);
+        } else {
+          message.channel.send("your Battle.net tag is incorrectly formatted -- please give in the form of [username]#[numbers]");
+          return;
+        }
+      }
+      //console.log(platform, region, tag)
+      fetch(`https://ow-api.com/v1/stats/${platform}/${region}/${tag}/profile`)
         .then(res => res.json())
         .then(profile => {
+          // handle when people have 0 stats in either/both quick play and competitive
+          let qpWins = "N/A";
+          let compWins = "N/A";
+
+          if (Object.keys(profile.quickPlayStats) > 0){
+            qpWins = profile.quickPlayStats.games.won.toString();
+          };
+          if (Object.keys(profile.competitiveStats) > 0){
+            qpWins = profile.competitiveStats.games.won.toString();
+          };
+          // put together our fun and spicy message 
           const e = new Discord.MessageEmbed()
             .setTimestamp()
             .setColor('0xFA9C1D') // apparently the overwatch league orange
-            .setTitle(`Overwatch Profile: ${profile.name}`)
-            .setThumbnail(profile.icon)
+            .setAuthor(`Overwatch Profile: ${profile.name}`,profile.icon)
+            .setThumbnail("https://img.pngio.com/overwatch-logo-icon-225668-free-icons-library-overwatch-icon-png-256_256.jpg")
             .addField('Level',`${profile.level}`,true)
             .addField('Prestige',`${profile.prestige}`,true)
             .addField('Rating',`${profile.rating}`,true)
-            .addField('Games Won',`${profile.gamesWon}`,true);
-        })
+            .addField('Quick Play Wins',qpWins,true)
+            .addField('Competitive Wins',compWins,true)
+            .addField('Total Wins',`${profile.gamesWon}`,true);
+          message.channel.send(e);
+        }).catch(e => console.error(e));
     };
     
 	},
